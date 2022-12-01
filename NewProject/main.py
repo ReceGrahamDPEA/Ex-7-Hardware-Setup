d42f5cb44e5056ea66f3bfd6d81b5981d49231fb
Rece Graham <recegraham@dpengineering.org>
2022-12-01 14:04:58 -0800

    Updated functionality. Still working on some buttons and the culmative "dance"

--- NewProject/main.py
+++ NewProject/main.py
@@ -28,7 +28,7 @@ from Slush.Devices import L6470Registers
 
 
 s0 = stepper(port=0, micro_steps=32, hold_current=20, run_current=20, accel_current=20, deaccel_current=20,
-    steps_per_unit=200, speed=8)
+    steps_per_unit=200, speed=2)
 
 
 time = datetime
@@ -63,21 +63,28 @@ Window.clearcolor = (1, 1, 1, 1)  # White
 
 class MainScreen(Screen):
 
-    """Things that are actually happening when the MainScreen class is called"""
+    """Things that are actually happening when the MainScreen class is called
+    These variables are only defined here so they can be altered later.
+    s0_rotation_dierction controls the rotation direction and stays between 1 and 0.
+    clock_control helps control the clock, as if the_dance() has been called the variable should update
+    and cancel the clock until the value is returned to 0, which the_dance function does when it is finished running"""
 
     s0_rotation_direction = 0
+    clock_control = 0
 
 
     def __init__(self, **kw):
         super().__init__(**kw)
 
-        Clock.schedule_interval(self.speed_change, 0.002)
+        Clock.schedule_interval(self.speed_change, 0.02)
+
+        print("clock_control value " + str(self.clock_control))
 
 
     def set_home(self):
 
         "Sets the home of the stepper motor in port 0"
-
+        
         s0.set_as_home()
         print("set as home")
 
@@ -86,8 +93,11 @@ class MainScreen(Screen):
 
         "Tells the motor in port 0 to go to its home"
 
+        self.clock_control += 1
         s0.goHome()
         print("go home")
+        s0.wait_move_finish()
+        self.clock_control -= 1
 
 
     def get_pos(self):
@@ -111,28 +121,38 @@ class MainScreen(Screen):
 
     def change_direction(self):
 
-        if self.s0_rotation_direction == 0:
-            self.s0_rotation_direction += 1
-            print(int(self.s0_rotation_direction))
+        if s0.is_busy() == True:
+            if self.s0_rotation_direction == 0:
+                self.s0_rotation_direction += 1
+                print("direction " + str(self.s0_rotation_direction))
 
-        else:
-            self.s0_rotation_direction -= 1
-            print(int(self.s0_rotation_direction))
+            else:
+                self.s0_rotation_direction -= 1
+                print("direction " + str(self.s0_rotation_direction))
 
-        s0.go_until_press(self.s0_rotation_direction, self.ids.speed_slider.value)
+            s0.go_until_press(self.s0_rotation_direction, self.ids.speed_slider.value)
 
 
     def speed_change(self, dt):
 
-        s0.go_until_press(self.s0_rotation_direction, self.ids.speed_slider.value)
+        if self.clock_control == 0:
+            if s0.is_busy() == True:
+                s0.go_until_press(self.s0_rotation_direction, self.ids.speed_slider.value)
 
 
     def the_dance(self):
+        sleep(.01)
+
+        self.clock_control += 1
 
         s0.get_position_in_units()
-        s0.go_until_press(0, 6400)
+        s0.start_relative_move(5)
+        s0.wait_move_finish()
 
-
+        s0.free()
+        self.clock_control -= 1
+        print("dance over")
+        
 
     def soft_stop(self):
 
@@ -149,12 +169,21 @@ class MainScreen(Screen):
         s0.free_all()
         print("freedom!")
 
+
     @staticmethod
     def exit_program():
 
         s0.free_all()
         print("freedom!")
         quit()
+
+
+
+
+
+
+
+
 
 
 class AdminScreen(Screen):
